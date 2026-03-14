19d28f8b2c9312543f793f81232d2809ebeb17eb
Oliver Tautz <oliver_tautz_yahoo.de>
2026-03-14 11:16:00 +0100

    feat: add image field type and fix code formatting
    - Add `image` to FieldType enum to support image fields in trackers
    - Reformat Field class constructor to single-line style
    - Add missing newlines at end of files per Dart conventions
    - Condense FieldType enum to single-line format
    
    This change extends the field type system to support image data while
    improving code consistency across model files.

--- brilliant_track/lib/models/field.dart
+++ brilliant_track/lib/models/field.dart
@@ -1,15 +1,8 @@
-enum FieldType {
-  integer,
-  float,
-  text,
-}
+enum FieldType { integer, float, text, image }
 
 class Field {
   final String name;
   final FieldType type;
 
-  Field({
-    required this.name,
-    required this.type,
-  });
+  Field({required this.name, required this.type});
 }
